ce5fd8b7270ad816b1a503dbdac65ff699b8b1a9
jppokedev <jppokedev@gmail.com>
2026-02-12 11:49:01 -0300

    correcting category association

--- backend/src/main/java/com/bitworld/dscatalog/entities/Category.java
+++ backend/src/main/java/com/bitworld/dscatalog/entities/Category.java
@@ -1,16 +1,11 @@
 package com.bitworld.dscatalog.entities;
 
 import java.time.Instant;
+import java.util.HashSet;
 import java.util.Objects;
+import java.util.Set;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.PrePersist;
-import jakarta.persistence.PreUpdate;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 
 @Entity
 @Table(name = "tb_category")
@@ -26,6 +21,9 @@ public class Category {
 
 	@Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
 	private Instant updatedAt;
+
+	@ManyToMany(mappedBy = "categories")
+	private Set<Product> products = new HashSet<>();
 
 	public Category() {
 	}
@@ -57,6 +55,10 @@ public class Category {
 
 	public Instant getUpdatedAt() {
 		return updatedAt;
+	}
+
+	public Set<Product> getProducts() {
+		return products;
 	}
 
 	@PrePersist
